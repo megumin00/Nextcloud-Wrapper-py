92cab343996148ff6439f9072f772499933fa84b
Tin Ngai Ronnie Low <ronnie_low@student.aishk.edu.hk>
2021-03-17 22:24:23 +0800

    added main file

---
+++ wrapper.py
@@ -0,0 +1,12 @@
+import requests
+
+class nc_wrapper():
+    def __init__(self):
+        pass
+    
+    def run(self):
+        pass
+    
+if __name__ == '__main__':
+    nc_wrapper = nc_wrapper()
+    nc_wrapper.run()
